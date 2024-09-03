56fddae41075fca223674045825985bc6baa4f2a
kunle babatunde <kb2232@nyu.edu>
2024-09-03 06:21:43 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kb2232/kunlegoutils
+
+go 1.22.3
---
+++ userinput.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+)
+
+func GetUserInputs(promptMsg string) (string, error) {
+	fmt.Print(promptMsg)
+	var value string
+	_, err := fmt.Scanln(&value)
+	if err != nil || value == "" {
+		println(err)
+		return "", errors.New("invalid input")
+	}
+	return value, nil
+}
